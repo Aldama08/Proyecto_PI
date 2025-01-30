dfd4e3ae0995001089f523332159ba479197ec40
Aldama LAP <jose.aldama5891@alumnos.udg.mx>
2025-01-29 22:28:35 -0600

    DONDE: intefaz +parte 1+ terminada

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
+
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Alert } from 'react-native';
+import Pantalla from './Pantalla';  // Ensure you're using the capitalized name
 
 export default function App() {
-  return (
+  return (  
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Pantalla />
+      <StatusBar style="light" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 0,
+    marginLeft: 0,
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#E2E0C8',
     alignItems: 'center',
-    justifyContent: 'center',
+    // justifyContent: 'center',
   },
 });
