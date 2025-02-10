6e19491a60523c583f4cfea9f56a3a3e69b56c04
Aldama LAP <jose.aldama5891@alumnos.udg.mx>
2025-02-10 10:51:00 -0600

    DONE:

--- Pantalla.js
+++ Pantalla.js
@@ -1,38 +1,65 @@
-
 import React, { Component } from "react";
-import { TextInput, View, Text, Image } from "react-native";
+import { TextInput, View, Text, Image, TouchableOpacity } from "react-native";
 
 export default class Pantalla extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      tempCiudad: "",
+      velViento: "",
+      buscarCiudad: "", 
+      PreCiudad: "",
     };
   }
+
+  buscar = () => {
+    const { buscarCiudad } = this.state;
+    const url = `http://api.weatherapi.com/v1/forecast.json?key=8e6389607b744bfc973152818250402&q=${buscarCiudad}&days=5&aqi=no&alerts=no`;
+
+    // Utilizando fetch(url))
+    fetch(url)
+      .then((response) => response.json()) // Convertir la respuesta a JSON
+      .then((data) => {
+        // console.log(data.current.temp_c);  Ver el valor en la consola
+
+        const temperatura = data.current.temp_c;
+        const velViento = data.current.wind_kph;
+        const PreCiudad = data.current.precip_mm;
+
+        // Actualizar el estado con los nuevos valores
+        this.setState({
+          tempCiudad: temperatura + "°C",
+          velViento: velViento + " km/h", 
+          PreCiudad: PreCiudad + " % ",
+
+        });
+      })
+      .catch((error) => {
+        console.error('Error fetching weather data:', error);
+      });
+  };
 
   render() {
     return (
       <View>
         <Text
-        style={{
+          style={{
             color: "#09122C",
-            fontSize: 50,
+            fontSize: 10,
             marginTop: 50,
             marginLeft: 40,
-        }}
+          }}
         >
-        App Tiempo
+          App Tiempo
         </Text>
 
-
         <View>
-        <Image
+          <Image
             source={require('./imagenes/nube.png')}
             style={{
-              width: 200,
+              width: 300,
               height: 200,
-            //marginTop: -400,
-              marginLeft: 40,
+              marginLeft: 0,
               borderWidth: 0,
             }}
           />
@@ -42,26 +69,27 @@ export default class Pantalla extends Component {
               color: "#09122C",
               fontSize: 69,
               marginTop: -140,
-              marginLeft: 100,
+              marginLeft: 80,
               fontFamily: "monospace",
             }}
           >
-            32°
+            {this.state.tempCiudad}
           </Text>
         </View>
 
         <View>
-            <TextInput
-            placeholder = "Ciudad a buscar"
-            keyboardType="numeric   "
+          <TextInput
+            onChangeText={(text) => this.setState({ buscarCiudad: text })}
+            placeholder="Ciudad a buscar"
+            keyboardType="default"
             style={{
-                color: "#09122C",
-                marginLeft: 120,
-                marginTop: 50,
-                fontSize: 30,
-                marginLeft: 40,
+              color: "#09122C",
+              marginLeft: 40,
+              marginTop: 50,
+              fontSize: 30,
             }}
-            />
+          />
+          <TouchableOpacity onPress={this.buscar}>
             <Image
               source={require('./imagenes/lupa.png')}
               style={{
@@ -69,11 +97,10 @@ export default class Pantalla extends Component {
                 height: 30,
                 marginTop: -37,
                 marginLeft: 230,
-                transform: [{rotate: '90deg'}],
-
+                transform: [{ rotate: '90deg' }],
               }}
-
             />
+          </TouchableOpacity>
         </View>
 
         <View>
@@ -94,10 +121,9 @@ export default class Pantalla extends Component {
               fontSize: 25,
               marginTop: 10,
               marginLeft: -15,
-
             }}
           >
-            32 km/h
+            {this.state.velViento}
           </Text>
 
           <Image
@@ -105,8 +131,8 @@ export default class Pantalla extends Component {
             style={{
               width: 80,
               height: 80,
-               marginTop: -120,
-               marginLeft: 190,
+              marginTop: -120,
+              marginLeft: 230,
             }}
           />
 
@@ -116,17 +142,13 @@ export default class Pantalla extends Component {
               fontFamily: "monospace",
               fontSize: 25,
               marginTop: 10,
-              marginLeft: 210,
-
+              marginLeft: 260,
             }}
           >
-            40%
+            {this.state.PreCiudad}
           </Text>
-          
         </View>
-
       </View>
     );
   }
 }
-
